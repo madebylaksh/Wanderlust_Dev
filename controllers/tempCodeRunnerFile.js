175f5eeea26a8c5f87c216cda99c27365568492a
Laksh Sharma <lksh.2468@gmail.com>
2026-08-04 13:11:04 +0530

    Delete controllers/tempCodeRunnerFile.js

--- controllers/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-let category = "LAJSDASJK DASJX ZXzxcnjzn nzxkj najskdnc jkzxn nsd sdncjvn k";
-category = category.toLowerCase().split(" ").map(e=> e.trim()[0].toUpperCase() + e.slice(1)).join(" ");
-console.log(category);
